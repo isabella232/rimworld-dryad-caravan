cea1301edc528c26026e308fd0b8fb60d65a48d3
Gareth <blackfireweb@gmail.com>
2022-01-17 18:42:54 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Dryad Caravan
+A small Rimworld mod that allows Dryads to go out on caravans
---
+++ Source/HarmonyPatches/Patch_DryadCaravan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI.Group;
+
+namespace Dryad_Caravan.HarmonyPatches
+{
+	[HarmonyPatch(typeof(PawnDiedOrDownedThoughtsUtility), nameof(PawnDiedOrDownedThoughtsUtility.TryGiveThoughts), new Type[] { typeof(Pawn), typeof(DamageInfo?), typeof(PawnDiedOrDownedThoughtsKind) })]
+	
+	public class Patch_DryadLost
+	{
+		public static void Prefix(Pawn victim,
+			DamageInfo? dinfo,
+			PawnDiedOrDownedThoughtsKind thoughtsKind)
+		{
+
+			if (thoughtsKind == PawnDiedOrDownedThoughtsKind.Lost)
+			{
+				victim.Kill(dinfo);
+			}
+		}
+	}
+}
